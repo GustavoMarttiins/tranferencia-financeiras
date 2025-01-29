ec7f9d7aab790bf99bc82886c63cd68384bd21a6
Martins <gustavo_marttiins@hotmail.com>
2025-01-29 15:39:01 -0300

    Iniciando novo projeto

---
+++ ms-transferencia-financeira/src/main/java/com/br/ms_transferencia_financeira/MsTransferenciaFinanceiraApplication.java
@@ -0,0 +1,13 @@
+package com.br.ms_transferencia_financeira;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsTransferenciaFinanceiraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsTransferenciaFinanceiraApplication.class, args);
+	}
+
+}
---
+++ ms-transferencia-financeira/src/test/java/com/br/ms_transferencia_financeira/MsTransferenciaFinanceiraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.ms_transferencia_financeira;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsTransferenciaFinanceiraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
